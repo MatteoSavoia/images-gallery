f5d5c2cab6212fcee0c15e9e904f050a87969588
Matteo Savoia <matteo.savoia@applied.it>
2023-07-14 10:38:10 +0200

    controlled serch component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import { Navbar } from 'react-bootstrap';
+import React from "react";
+import { Navbar, Container } from "react-bootstrap";
+
+const navbarStyle = {
+  backgroundColor: "silver",
+};
 
 const Header = ({ title }) => {
-    return (
-      <Navbar bg="dark" variant="light">
-          <Navbar.Brand href="/">{title}</Navbar.Brand>
-      </Navbar>
-    )
+  return (
+    <Navbar style={navbarStyle} variant="light">
+      <Container>
+        <Navbar.Brand href="/">{title}</Navbar.Brand>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Header;
